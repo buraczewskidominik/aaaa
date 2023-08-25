f6569e86d85fe120ffbb03703f9248780d1bea42
Dominik Buraczewski <44263354+buraczewskidominik@users.noreply.github.com>
2023-08-25 09:30:45 +0200

    Initial Commit

---
+++ aaaa/aaaaApp.swift
@@ -0,0 +1,17 @@
+//
+//  aaaaApp.swift
+//  aaaa
+//
+//  Created by Dominik Buraczewski on 25/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct aaaaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
